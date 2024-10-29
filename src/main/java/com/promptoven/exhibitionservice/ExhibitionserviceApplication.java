4bdce04ece6fdb1735a1d3a4df7095beeff836fc
jaehunLee-git <joz526@naver.com>
2024-10-29 09:52:20 +0900

    first commit

---
+++ src/main/java/com/promptoven/exhibitionservice/ExhibitionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.promptoven.exhibitionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExhibitionserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/promptoven/exhibitionservice/ExhibitionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promptoven.exhibitionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExhibitionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
