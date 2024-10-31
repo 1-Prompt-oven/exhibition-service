42d8e7d3349899d4eac296cbaab90b588c87a1ba
jaehunLee-git <joz526@naver.com>
2024-10-31 15:07:05 +0900

    Refactor : 기획전 조회 API 수정

--- src/main/java/com/promptoven/exhibitionservice/member/exhibition/infrastructure/ExhibitionRepository.java
+++ src/main/java/com/promptoven/exhibitionservice/member/exhibition/infrastructure/ExhibitionRepository.java
@@ -3,5 +3,9 @@ package com.promptoven.exhibitionservice.member.exhibition.infrastructure;
 import com.promptoven.exhibitionservice.common.domain.Exhibition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExhibitionRepository extends JpaRepository<Exhibition, Long> {
+
+    List<Exhibition> findAllByDeletedFalse();
 }
